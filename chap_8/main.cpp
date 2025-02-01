8ee71881548e2a3ed1e7185dd753ea98beda9791
satoshi_kamiya <satoshi_kamiya@aiphone.co.jp>
2025-02-01 17:12:54 +0900

    ループ

---
+++ chap_8/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std::literals;
+
+int main()
+{
+    for ( int a = 1 ; a <= 9 ; ++a )
+    {
+        for ( int b = 1 ; b <= 9 ; ++b )
+        { std::cout << a*b << "\t"s ; }
+
+        std::cout << "\n"s ;
+    }
+}
