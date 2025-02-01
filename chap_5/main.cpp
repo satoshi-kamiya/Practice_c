8c40e0884cd70d519433e37a5729cdc23a7e8c51
satoshi_kamiya <satoshi_kamiya@aiphone.co.jp>
2025-02-01 16:27:54 +0900

    条件分岐の果てのレストラン

---
+++ chap_5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std::literals;
+
+int main()
+{
+    auto a = 1 == 1;
+    auto b = 1 != 1;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    
+
+
+}
