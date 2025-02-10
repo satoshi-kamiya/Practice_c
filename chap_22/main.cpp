e69c1fb3eb75d0948b2296f1a7b9a73ec87e8537
satoshi_kamiya <satoshi_kamiya@aiphone.co.jp>
2025-02-10 13:23:02 +0900

    配列

---
+++ chap_22/main.cpp
@@ -0,0 +1,16 @@
+struct array_int_10
+{
+    int storage[10] ;
+
+    int & operator [] ( std::size_t i )
+    {
+        return storage[i] ;
+    }
+} ;
+
+int main()
+{
+    array_int_10 a = {0,1,2,3,4,5,6,7,8,9} ;
+    a[3] = 0 ;
+    std::cout << a[6] ;
+}
